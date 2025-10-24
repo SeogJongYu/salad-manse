504515744d7528c3407e8784a2f0e371c1051477
SeogjongYu <seogjongyu@gmail.com>
2025-10-24 21:17:41 +0900

    chore(data): data seed 추가

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,6 +6,11 @@ import { ingredientTags } from './data/ingredientTags';
 const prisma = new PrismaClient();
 
 // async function main() {
+//   // ID 시퀀스 값을 현재 테이블의 최대 ID 값으로 되돌리는 SQL 명령
+//   // await prisma.$executeRawUnsafe(
+//   //   'SELECT setval(\'"Ingredient_id_seq"\', COALESCE((SELECT MAX(id) FROM "Ingredient"), 0), true);',
+//   // );
+
 //   await prisma.ingredient.createMany({
 //     data: ingredients,
 //     skipDuplicates: true,
